1b70e43472f62f23815d7ab4942d698567446f02
Anson Ng <cook200001@yahoo.com.hk>
2019-04-21 17:12:52 +0800

    add PyTorch Lecture 03: Gradient Descent

---
+++ PyTorch Lecture 03: Gradient Descent/Gradient.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x_data = [1.0, 2.0, 3.0]
+y_data = [2.0, 4.0, 6.0]
+
+w = 1.0
+
+def forward(x):
+    return x * w
+
+def loss(x, y):
+    y_pred = forward(x)
+    return (y_pred - y) * (y_pred - y)
+
+#03
+def gradient(x, y):
+    return 2 * x * ( x * w - y)
+
+print ("predict (brefore training)", 4, forward(4))
+
+# Training loop
+for epoch in range(1000):
+    for x_val, y_val in zip(x_data, y_data):
+        grad = gradient(x_val, y_val)
+        w = w - 0.01 * grad
+        print("\tgrad:", x_val, y_val, grad)
+        l = loss(x_val, y_val)
+    print ("progress:", epoch, "w=", w, "loss=", l)
+
+
+print ("predict (after training)", "4 hours", forward(4))
--- README.md
+++ README.md
@@ -3,14 +3,10 @@ pyTorchStudy
 
 https://pytorch.org/
 
-```
-import torch
-print(torch.__version__)
-```
 
 # PyTorch Lecture 02: Linear Model
 https://www.youtube.com/watch?v=l-Fe9Ekxxj4
-```
-PyTorch Lecture 02: Linear Model/LinerModel.py
-```
+
+# PyTorch Lecture 03: Gradient Descent
+https://www.youtube.com/watch?v=b4Vyma9wPHo
 
